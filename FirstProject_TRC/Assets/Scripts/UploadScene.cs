d5d7453d419d9215d69eee4c482cbd21163c066d
JuniorMartinez17 <45376053+JuniorMartinez17@users.noreply.github.com>
2018-11-27 14:23:14 -0500

    Menu

---
+++ FirstProject_TRC/Assets/Scripts/UploadScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UploadScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SceneToUpload(string Name_scene_Upload)
+    {
+        SceneManager.LoadScene(Name_scene_Upload);
+    }
+}
