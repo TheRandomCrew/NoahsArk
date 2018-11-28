f306f9e90c9fb92938bc148ff81f4f5325b2fc08
JuniorMartinez17 <45376053+JuniorMartinez17@users.noreply.github.com>
2018-11-28 18:22:57 -0500

    Script Physics, Configuration Joystick
    Se creo el script para el control de la fisica del personaje. Se añadio la animación de caminar para trabajar un poco sobre ella, hay que esperar las animaciones en un solo objectCharacter para pulir un poco. Se importo la funcionalidad del joystick y se configuro un poco, (Hay que debatir como serán los controles).

---
+++ FirstProject_TRC/Assets/Scripts/ControllerCharacter/Physics.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics : MonoBehaviour {
+
+    public float speed;
+
+    private Animator animator;
+    private Rigidbody rigibody;
+
+
+    private void Awake()
+    {
+        rigibody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        animator.SetBool("IsWalking", false);
+        if (moveHorizontal != 0)
+        {
+            if (moveHorizontal < 0) gameObject.transform.localScale = new Vector3(1f, 1f, -1f);
+            else if (moveHorizontal > 0) gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+            animator.SetBool("IsWalking", true);
+            
+        }
+        Vector3 movement = new Vector3(moveHorizontal, 0f, 0f);
+        rigibody.velocity = movement * speed;
+
+    }
+}
