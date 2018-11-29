ef53544d118827e40a7ca4a3b44ae243bc33f1ab
JuniorMartinez17 <45376053+JuniorMartinez17@users.noreply.github.com>
2018-11-28 23:35:43 -0500

    Create Animations Character + Physics
    Se crearon las animaciones del personaje con algunas de sus fisicas. Falta corregir algunos bugs en los movimientos. Se creo el script del meteorito, por ahora vacio.

--- FirstProject_TRC/Assets/Scripts/ControllerCharacter/Physics.cs
+++ FirstProject_TRC/Assets/Scripts/ControllerCharacter/Physics.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Physics : MonoBehaviour {
+        
+    public Joystick joystick;
 
-    public float speed;
-
+    [SerializeField] private float health = 100f;
+    [SerializeField] private float speed;
     private Animator animator;
-    private Rigidbody rigibody;
-
+    private Rigidbody rigibody;    
 
     private void Awake()
     {
@@ -18,17 +19,43 @@ public class Physics : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        animator.SetBool("IsWalking", false);
+
+        //Physics Movement in axis x
+        float moveHorizontal = joystick.Horizontal;
+
         if (moveHorizontal != 0)
         {
-            if (moveHorizontal < 0) gameObject.transform.localScale = new Vector3(1f, 1f, -1f);
-            else if (moveHorizontal > 0) gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
-            animator.SetBool("IsWalking", true);
-            
+            gameObject.transform.localScale = new Vector3(1f, 1f, 1f*(Mathf.Sign(moveHorizontal)));
+
+            if (Mathf.Abs(moveHorizontal) < 0.4f)
+            {
+                animator.SetBool("IsWalking", true);
+                animator.SetBool("IsRunning", false);
+                speed = 2f;
+            } else
+            {
+                animator.SetBool("IsRunning", true);
+                animator.SetBool("IsWalking", false);
+                speed = 8f;
+            }           
+
+        } else
+        {
+            animator.SetBool("IsWalking", false);
+            animator.SetBool("IsRunning", false);
+            speed = 0;
         }
+
         Vector3 movement = new Vector3(moveHorizontal, 0f, 0f);
         rigibody.velocity = movement * speed;
+
+        //Physics Movement in axis x
+        float moveVertical = joystick.Vertical;
+
+        if (moveVertical >= 0.5f)
+        {
+            animator.SetTrigger("IsJumping");
+        }
 
     }
 }
